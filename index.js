677d70f13083db766b5b96adbdd7f7374db2b33c
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-06-14 21:22:20 +0200

    updated the js file

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ function addTask() {
   const taskText = taskInput.value.trim(); //the trim removes excess whitespace
   if (taskText === "") return;
 
-  const li = document.createElement("li");
+  const li = document.createElement("li"); //this is to create the li element
   li.textContent = taskText;
+
+  const deleteBtn = document.createElement("button"); //this creates the delete button for list
+  deleteBtn.textContent = "Delete";
+  deleteBtn.addEventListener("click", deleteTask); //this line is call the function that will execute the delete function
 }
 
 const submitTaskButton = document.querySelector("#submit-btn");

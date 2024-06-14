da909b882e54d2c7e18f4a12257c61f5fd036731
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-06-14 21:26:24 +0200

    completed the addtask function

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ function addTask() {
   const deleteBtn = document.createElement("button"); //this creates the delete button for list
   deleteBtn.textContent = "Delete";
   deleteBtn.addEventListener("click", deleteTask); //this line is call the function that will execute the delete function
+  li.appendChild(deleteBtn);
+
+  li.addEventListener("click", completeTask);
+
+  const taskList = document.querySelector("taskList");
+  taskList.appendChild(li); //this creates the child li element of the parent ul element
+
+  taskInput.value = "";
 }
 
 const submitTaskButton = document.querySelector("#submit-btn");
